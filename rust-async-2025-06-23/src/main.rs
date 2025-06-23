688dd7e4610dc7f2c7173aac73eb47b6b7a96548
pollenjp <polleninjp@gmail.com>
2025-06-23 17:03:47 +0900

    update

--- rust-async-2025-06-23/src/main.rs
+++ rust-async-2025-06-23/src/main.rs
@@ -1,4 +1,23 @@
+use tokio::{
+    spawn,
+    time::{Duration, sleep},
+};
+
+async fn say_hello() {
+    // Wait for a while before printing to make it a more interesting race.
+    sleep(Duration::from_millis(100)).await;
+    println!("hello");
+}
+
+async fn say_world() {
+    sleep(Duration::from_millis(100)).await;
+    println!("world!");
+}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    spawn(say_hello());
+    spawn(say_world());
+    // Wait for a while to give the tasks time to run.
+    sleep(Duration::from_millis(1000)).await;
 }

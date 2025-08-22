532e8b30aa6885e552144ba46dd5c2ce3d8b4f46
pollenjp <polleninjp@gmail.com>
2025-08-22 11:40:22 +0900

    add console_error_panic_hook

--- rust-leptos-2025-08-22/src/main.rs
+++ rust-leptos-2025-08-22/src/main.rs
@@ -1,5 +1,9 @@
+use console_log::init_with_level;
 use leptos::prelude::*;
 
 fn main() {
+    _ = init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
     leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
 }

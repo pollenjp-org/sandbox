3f3eb78253ba915bd98e708ffdf1bdac5f613eb6
pollenjp <polleninjp@gmail.com>
2025-09-27 01:51:43 +0900

    feat(ebpf): update

--- ebpf-2025-09-25/main.py
+++ ebpf-2025-09-25/main.py
@@ -2,12 +2,26 @@
 #
 # Use system python because bcc is installed in system python
 
+from time import sleep
+
 from bcc import BPF
 
 program = r"""
+BPF_HASH(counter_table);
+
 int hello(void *ctx) {
-    bpf_trace_printk("Hello World!");
-    return 0;
+   u64 uid;
+   u64 counter = 0;
+   u64 *p;
+
+   uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+   p = counter_table.lookup(&uid);
+   if (p != 0) {
+      counter = *p;
+   }
+   counter++;
+   counter_table.update(&uid, &counter);
+   return 0;
 }
 """
 
@@ -15,4 +29,12 @@ b = BPF(text=program)
 syscall = b.get_syscall_fnname("execve")
 b.attach_kprobe(event=syscall, fn_name="hello")
 
-b.trace_print()
+# Attach to a tracepoint that gets hit for all syscalls
+# b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
+
+while True:
+    sleep(2)
+    s = ""
+    for k, v in b["counter_table"].items():
+        s += f"ID {k.value}: {v.value}\t"
+    print(s)
